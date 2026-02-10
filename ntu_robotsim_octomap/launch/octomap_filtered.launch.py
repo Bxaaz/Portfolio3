455f9193d8f4290729d5780852d9698f834b6cbb
NTU-User <ntu-user@ntu.ac.uk>
2026-02-10 20:39:56 +0000

    Basic Tasks nearly all done

--- ntu_robotsim_octomap/launch/octomap_filtered.launch.py
+++ ntu_robotsim_octomap/launch/octomap_filtered.launch.py
@@ -1,32 +1,41 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+
+    octomap_launch = PathJoinSubstitution([
+        FindPackageShare('octomap_server2'),
+        'launch',
+        'octomap_server_launch.py'
+    ])
+
     return LaunchDescription([
 
-        Node(
-            package='octomap_server2',
-            executable='octomap_server',
-            name='octomap_server',
-            output='screen',
-            parameters=[{
-                'resolution': 0.05,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(octomap_launch),
+            launch_arguments={
+
+                'input_cloud_topic': '/rgbd_camera/points',
+                'resolution': '0.05',
+
                 'frame_id': 'odom',
                 'base_frame_id': 'base_link',
 
-                'filter_ground': True,
-                'ground_filter/distance': 0.04,
-                'ground_filter/angle': 0.15,
-                'ground_filter/plane_distance': 0.07,
+                'filter_ground': 'True',
+                'filter_speckles': 'True',
 
-                'publish_markers': True,
-                'publish_pointcloud': True,
-                'publish_free_space': True,
+                'ground_filter/distance': '0.04',
+                'ground_filter/angle': '0.15',
+                'ground_filter/plane_distance': '0.20',
 
-                'sensor_model/max_range': 5.0,
-            }],
-            remappings=[
-                ('cloud_in', '/rgbd_camera/points')
-            ]
+                'sensor_model/max_range': '6.0',
+
+                'publish_free_space': 'True',
+                'incremental_2D_projection': 'True'
+
+            }.items()
         )
     ])
